9736adf71a7ebcb5789c0461e35982a6500abc71
José Carlos de Moraes Filho <josecarlosdemoraesfilho@gmail.com>
2019-03-22 19:56:04 -0300

    object short sintax

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const nome  = 'José'
 const idade = 25
 
-console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
+const usuario = {
+  nome,
+  idade,
+  empresa:'Mson'
+}
+
+console.log(usuario)
+
+/* No ES6+ quando a propriedade de um objeto possui o mesmo nome que uma variável a ser configurada, não é necessário a repetir o nome da variável. Vale lembrar que ambos devem estar no mesmo escopo. */
