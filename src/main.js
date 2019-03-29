26b941d44ff7c201f014f2af3bf16100bec97189
José Carlos de Moraes Filho <josecarlosdemoraesfilho@gmail.com>
2019-03-29 14:53:50 -0300

    Async Await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-alert('Opa')
+const minhaPromise = () => new Promise((resolve, reject)=>{
+  setTimeout(()=>{resolve('Ok')}, 2000)
+})
+
+async function executaPromise(){
+  const response = await minhaPromise();
+
+  console.log(response)
+}
+
+executaPromise()
+
+const executaPromise2 = async() => {
+  console.log(await minhaPromise())
+  console.log(await minhaPromise())
+  console.log(await minhaPromise())
+}
+
+executaPromise2()
