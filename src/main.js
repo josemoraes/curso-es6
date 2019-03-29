57f27a62c1cece922448e31a28c1635248112eb4
José Carlos de Moraes Filho <josecarlosdemoraesfilho@gmail.com>
2019-03-29 17:54:40 -0300

    Aplicando os conhecimentos
    #2

---
+++ src/main.js
@@ -0,0 +1,53 @@
+class App{
+  constructor(){
+    this.repositories = []
+    this.formEl = document.getElementById("repo-form")
+    this.listEl = document.getElementById("repo-list")
+
+    this.registerHandlers()
+  }
+
+  registerHandlers(){
+    this.formEl.onsubmit = event => this.addRepository(event)
+  }
+
+  addRepository(event){
+    event.preventDefault()
+
+    this.repositories.push({
+      name: 'rocketseat.com.br',
+      description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url:'http://github.com/rocketseat/rocketseat.com.br'
+    })
+
+    this.render()
+  }
+
+  render(){
+    this.listEl.innerHTML = ""
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img')
+      let titleEl = document.createElement('strong')
+      let descriptionEl = document.createElement('p')
+      let linkEl = document.createElement('a')
+      let listItemEl = document.createElement('li')
+
+      imgEl.setAttribute('src', repo.avatar_url)
+      titleEl.appendChild(document.createTextNode(repo.name))
+      descriptionEl.appendChild(document.createTextNode(repo.description))
+      linkEl.appendChild(document.createTextNode('Acessar'))
+      linkEl.setAttribute('target', '_blank')
+      linkEl.setAttribute('href', repo.html_url)
+
+      listItemEl.appendChild(imgEl)
+      listItemEl.appendChild(titleEl)
+      listItemEl.appendChild(descriptionEl)
+      listItemEl.appendChild(linkEl)
+
+      this.listEl.appendChild(listItemEl)
+    })
+  }
+}
+
+new App()
